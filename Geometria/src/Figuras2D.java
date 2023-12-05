9cf1e44f36621ee7d9da1ef2b1817d19f74a6432
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2023-12-05 09:36:16 -0500

    Update Figuras2D.java
    Implementacion de metodo perimtero

--- Geometria/src/Figuras2D.java
+++ Geometria/src/Figuras2D.java
@@ -1,2 +1,12 @@
-public class Figuras2D {
+public class Figuras2D extends Figuras{
+    public Figuras2D(int n, double lados, String nombre) {
+        super(n, lados, nombre);
+    }
+    public void perimetro (){
+        double peri = 0;
+        for (int i = 1; i<=getN(); i++){
+            peri += getLados();
+        }
+        System.out.println("El perímetro es:" + peri);
+    }
 }
