6c518c66c420cc1e99e53b2c6fb5b1b6cda6ef07
KevinDaniel10 <kevin.cola@epn.edu.ec>
2023-12-06 23:01:30 -0500

    Clase triangulo

--- Geometria/src/TrianguloEquilatero.java
+++ Geometria/src/TrianguloEquilatero.java
@@ -1,2 +1,18 @@
-public class TrianguloEquilatero {
+public class TrianguloEquilatero extends Regulares {
+    public TrianguloEquilatero(int n, double lados, String nombre) {
+        super(n, lados, nombre);
+    }
+    double areadeltriangulo;
+
+    public double getAreadeltriangulo() {
+        return areadeltriangulo;
+    }
+
+    public void setAreadeltriangulo(double areadeltriangulo) {
+        this.areadeltriangulo = areadeltriangulo;
+    }
+
+    public void areat(){
+        System.out.println("El area es: "+(this.getLados()*this.getLados()*Math.sqrt(3)/4));
+    }
 }
