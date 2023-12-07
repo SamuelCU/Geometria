2819c68f6a1d25260e9cf207251991a2d9d1ad5f
KevinDaniel10 <kevin.cola@epn.edu.ec>
2023-12-06 23:09:10 -0500

    Clases

--- Geometria/src/Hexagono.java
+++ Geometria/src/Hexagono.java
@@ -1,2 +1,21 @@
-public class Hexagono {
+public class Hexagono extends TrianguloEquilatero{
+
+    public Hexagono(int n, double lados, String nombre) {
+        super(n, lados, nombre);
+    }
+
+    double areah;
+
+    public double getAreah() {
+        return areah;
+    }
+
+    public void setAreah(double areah) {
+        this.areah = areah;
+    }
+
+    public void areahex(){
+        areah=areadeltriangulo*6;
+        System.out.println("El area del hexgono es: "+areah);
+    }
 }
--- Geometria/src/Pentagono.java
+++ Geometria/src/Pentagono.java
@@ -1,2 +1,19 @@
-public class Pentagono {
+public class Pentagono extends TrianguloEquilatero{
+    public Pentagono(int n, double lados, String nombre) {
+        super(n, lados, nombre);
+    }
+    double areap;
+
+    public double getAreap() {
+        return areap;
+    }
+
+    public void setAreap(double areap) {
+        this.areap = areap;
+    }
+
+    public void areape(){
+        areap=areadeltriangulo*5;
+        System.out.println("El area del pentagono es : "+areap);
+    }
 }
